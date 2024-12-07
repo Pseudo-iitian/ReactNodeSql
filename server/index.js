f269c5652f5442445017de72a02af842628865c0
Abhishek Verma <109858266+Pseudo-iitian@users.noreply.github.com>
2024-12-07 17:01:10 +0000

    done

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require('express');
+const { sequelize } = require('./models');
 const app = express()
 const PORT = 3001;
 
+const db = require('./models');
 
+db.sequelize.sync().then( ()=>{
+    app.listen(PORT,()=>{
+        console.log("server is running on port", PORT);
+    });
+})
 
-app.listen(PORT,()=>{
-    console.log("server is running on port", PORT);
-});
